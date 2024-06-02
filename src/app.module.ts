59bd4bf45115ddd05722ee2fe2fb8b66693f6cad
Vivin Rajagopalan <vrajagopalan@adobe.com>
2024-06-02 13:14:16 -0600

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MatchModule } from './match/match.module';
+
+@Module({
+  imports: [AuthModule, UserModule, MatchModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
